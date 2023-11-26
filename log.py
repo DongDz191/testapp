facd67df3d2f4ab3f09cd64c389d4fc8c5569618
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-26 23:52:06 +0700

    Create log.py

---
+++ log.py
@@ -0,0 +1,8 @@
+import os
+
+if st.button('Đăng nhập'):
+    if username == 'admin' and password == '123':
+        st.success('Đăng nhập thành công!')
+        os.startfile('app.py')
+    else:
+        st.error('Tên người dùng hoặc mật khẩu không đúng.')
