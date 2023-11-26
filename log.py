6fa367346d79761ba702e5f5995418f6fce0b9e2
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-26 23:56:58 +0700

    Update log.py

--- log.py
+++ log.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import os
+
+username = st.text_input('Tên người dùng')
+password = st.text_input('Mật khẩu', type='password')
 
 if st.button('Đăng nhập'):
     if username == 'admin' and password == '123':
