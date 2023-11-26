614ff006aedc2e1fd3a33afe12b80ad9f8d14e04
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:20:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import s3fs
 import streamlit as st
 
-# Khởi tạo S3FileSystem
-fs = s3fs.S3FileSystem(key='YOUR_ACCESS_KEY', secret='YOUR_SECRET_KEY')
+def main_app():
+    st.title('Main Application')
+    st.write("Welcome to the main application!")
 
-def upload_file_to_s3(file, bucket_name, file_path):
-    with fs.open(f'{bucket_name}/{file_path}', 'wb') as f:
-        f.write(file.getvalue())
-    return f's3://{bucket_name}/{file_path}'
+    # Khởi tạo S3FileSystem
+    fs = s3fs.S3FileSystem(key='YOUR_ACCESS_KEY', secret='YOUR_SECRET_KEY')
 
-# Giao diện người dùng để tải lên tệp tin
-st.title('Tải Tệp lên AWS S3')
+    def upload_file_to_s3(file, bucket_name, file_path):
+        with fs.open(f'{bucket_name}/{file_path}', 'wb') as f:
+            f.write(file.getvalue())
+        return f's3://{bucket_name}/{file_path}'
 
-uploaded_file = st.file_uploader("Chọn một tệp tin")
-if uploaded_file is not None:
-    file_path = upload_file_to_s3(uploaded_file, 'your-bucket-name', uploaded_file.name)
-    st.write(f"Tệp tin đã được tải lên thành công: {file_path}")
+    # Giao diện người dùng để tải lên tệp tin
+    st.title('Tải Tệp lên AWS S3')
+
+    uploaded_file = st.file_uploader("Chọn một tệp tin")
+    if uploaded_file is not None:
+        file_path = upload_file_to_s3(uploaded_file, 'your-bucket-name', uploaded_file.name)
+        st.write(f"Tệp tin đã được tải lên thành công: {file_path}")
