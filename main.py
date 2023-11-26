b441e1bec40ce9621bb34d7b7db87c054a208a24
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:54:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import streamlit as st
-from log import login
-from app import main_app
 
-login()
+def main():
+    # Tạo placeholder
+    login_placeholder = st.empty()
 
-if 'login_status' not in st.session_state:
-    st.session_state['login_status'] = False
-else:
-    st.session_state['login_status'] = True
+    # Hiển thị form login
+    with login_placeholder:
+        login_form = st.form(key='login_form')
+        username = login_form.text_input('Tên đăng nhập')
+        password = login_form.text_input('Mật khẩu', type='password')
+        submit_button = login_form.form_submit_button('Đăng nhập')
 
-if st.session_state['login_status']:
-    main_app()
+    # Kiểm tra xem người dùng đã đăng nhập chưa
+    if submit_button and username == 'admin' and password == '123':
+        # Xóa placeholder
+        login_placeholder.empty()
+
+        # Hiển thị form main_app()
+        with st.form(key='main_app'):
+            # Thêm các thành phần của form main_app() vào đây
+            pass
