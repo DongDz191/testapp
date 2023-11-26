f2fb24a4956bde6e648640bfc58bb49c2dc8bc7b
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-26 23:09:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
+import s3fs
 import streamlit as st
-import boto3
 
-# Tạo client S3
-s3_client = boto3.client('s3', aws_access_key_id='YOUR_ACCESS_KEY', aws_secret_access_key='YOUR_SECRET_KEY')
+# Khởi tạo S3FileSystem
+fs = s3fs.S3FileSystem(key='YOUR_ACCESS_KEY', secret='YOUR_SECRET_KEY')
 
-def upload_file_to_s3(file, bucket, object_name):
-    try:
-        s3_client.upload_fileobj(file, bucket, object_name)
-    except Exception as e:
-        return False, str(e)
-    return True, "File uploaded successfully"
+def upload_file_to_s3(file, bucket_name, file_path):
+    with fs.open(f'{bucket_name}/{file_path}', 'wb') as f:
+        f.write(file.getvalue())
+    return f's3://{bucket_name}/{file_path}'
 
 # Giao diện người dùng để tải lên tệp tin
 st.title('Tải Tệp lên AWS S3')
 
 uploaded_file = st.file_uploader("Chọn một tệp tin")
 if uploaded_file is not None:
-    success, message = upload_file_to_s3(uploaded_file, 'your-bucket-name', uploaded_file.name)
-    if success:
-        st.success(message)
-    else:
-        st.error(message)
+    file_path = upload_file_to_s3(uploaded_file, 'your-bucket-name', uploaded_file.name)
+    st.write(f"Tệp tin đã được tải lên thành công: {file_path}")
