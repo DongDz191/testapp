b111759a3e20358ef17937d6e02af4a0e8e869a2
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:52:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from app import main_app
 
 login()
 
+if 'login_status' not in st.session_state:
+    st.session_state['login_status'] = False
+else:
+    st.session_state['login_status'] = True
+
 if st.session_state['login_status']:
     main_app()
