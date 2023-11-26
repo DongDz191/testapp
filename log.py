0f6c9ced6e259199b593cea23559e5622f3e06f6
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:17:51 +0700

    Update log.py

--- log.py
+++ log.py
@@ -1,12 +1,13 @@
 import streamlit as st
-import os
 
-username = st.text_input('Tên người dùng')
-password = st.text_input('Mật khẩu', type='password')
-
-if st.button('Đăng nhập'):
-    if username == 'admin' and password == '123':
-        st.success('Đăng nhập thành công!')
-        os.startfile('/app.py')
-    else:
-        st.error('Tên người dùng hoặc mật khẩu không đúng.')
+def login():
+    st.title('Login Page')
+    username = st.text_input('Username')
+    password = st.text_input('Password', type='password')
+    
+    if st.button('Login'):
+        if username == "admin" and password == "12345":
+            return True
+        else:
+            st.error("Incorrect username or password")
+            return False
