d8e3bd2a65492516d5f64134e165f81b2685d7fb
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:50:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,7 @@ import streamlit as st
 from log import login
 from app import main_app
 
-# Check login status
-if 'login_status' not in st.session_state:
-    st.session_state['login_status'] = False
+login()
 
 if st.session_state['login_status']:
-    # Hide the login form
-    st.write("You are logged in!")
     main_app()
-else:
-    # Display the login form
-    if login():
-        st.session_state['login_status'] = True
-        main_app()
