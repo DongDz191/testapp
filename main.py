89bac42e72b1bdfc15599b3790abdf4f12ba3c51
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:27:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from app import main_app
 if 'login_status' not in st.session_state:
     st.session_state['login_status'] = False
 
+# Create a placeholder for the login page
+login_placeholder = st.empty()
+
 if st.session_state['login_status']:
     main_app()
 else:
-    if login():
+    # Use the placeholder to display the login page
+    if login_placeholder.login():
         st.session_state['login_status'] = True
+        # Clear the login page once the user logs in successfully
+        login_placeholder.empty()
         main_app()
