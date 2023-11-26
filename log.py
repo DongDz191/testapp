cd8fd35bd836a786848e902cf835753af3143437
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:06:59 +0700

    Update log.py

--- log.py
+++ log.py
@@ -7,6 +7,6 @@ password = st.text_input('Mật khẩu', type='password')
 if st.button('Đăng nhập'):
     if username == 'admin' and password == '123':
         st.success('Đăng nhập thành công!')
-        os.startfile('./app.py')
+        os.startfile('/app.py')
     else:
         st.error('Tên người dùng hoặc mật khẩu không đúng.')
