7c463b5be6ee995fe4669212fac80ba74faeaf66
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 01:12:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,15 @@ from app import main_app
 if 'login_status' not in st.session_state:
     st.session_state['login_status'] = False
 
-# Create a placeholder for the login page
-login_placeholder = st.empty()
-
 # If already logged in, show the main app
 if st.session_state['login_status']:
     main_app()
 else:
-    # Show the login form in the placeholder
-    with login_placeholder.container():
+    # Create a separate container for the login
+    with st.container():
         if login():
-            # On successful login, clear the placeholder and display the main app
-            login_placeholder.empty()
+            # On successful login, update login_status and rerun the app
             st.session_state['login_status'] = True
-            main_app()
+            st.experimental_rerun()
+
+    # You can add additional content below the login container if needed
