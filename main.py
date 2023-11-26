f0eb2111eba1dd3d47a3004cc3432c310c327aab
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:46:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if 'login_status' not in st.session_state:
 if st.session_state['login_status']:
     # Hide the login form
     st.write("You are logged in!")
+    main_app()
 else:
     # Display the login form
     if login():
