b9b01ab43f4b5b276fc303d1f46a9684aeb605e3
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 01:11:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import streamlit as st
 from log import login
 from app import main_app
 
-placeholder = st.empty()
-
-# Kiểm tra trạng thái đăng nhập
+# Initialize login_status in session_state if not already present
 if 'login_status' not in st.session_state:
     st.session_state['login_status'] = False
 
+# Create a placeholder for the login page
+login_placeholder = st.empty()
+
+# If already logged in, show the main app
 if st.session_state['login_status']:
     main_app()
 else:
-    if login():
-        st.session_state['login_status'] = True
+    # Show the login form in the placeholder
+    with login_placeholder.container():
+        if login():
+            # On successful login, clear the placeholder and display the main app
+            login_placeholder.empty()
+            st.session_state['login_status'] = True
+            main_app()
