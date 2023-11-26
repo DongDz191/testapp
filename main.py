9032542eaf3fe6f3fcc2c0a8d5433f0a61d13e8c
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-27 00:38:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import streamlit as st
 from log import login
 from app import main_app
 
-# Create a placeholder for the login form
-login_placeholder = st.empty()
-
 # Check login status
 if 'login_status' not in st.session_state:
     st.session_state['login_status'] = False
@@ -12,8 +9,7 @@ if 'login_status' not in st.session_state:
 if st.session_state['login_status']:
     main_app()
 else:
-    # Display the login form in the placeholder
-    if login_placeholder.login():
+    # Display the login form
+    if login():
         st.session_state['login_status'] = True
-        login_placeholder.empty()  # Clear the login form
         main_app()
